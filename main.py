fdc1adc68d53b0701717894bdc385c7de79f4e9c
Paul Sawaya <me@paulsawaya.com>
2013-09-28 20:09:47 -0700

    beginning of facebook integration

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import logging
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return "Hello (shaky) world!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.config.update(DEBUG=True,PROPAGATE_EXCEPTIONS=True,TESTING=True)
