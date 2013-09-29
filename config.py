513a951210739fab15f553914a347b8668f49502
Paul Sawaya <me@paulsawaya.com>
2013-09-28 20:31:39 -0700

    talk to mongo

---
+++ config.py
@@ -0,0 +1,1 @@
+LOCAL_MONGO_URL="mongodb://localhost:27017/"
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from pymongo import MongoClient
+
+import config
 import logging
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.from_object(config)
+
+conn = MongoClient(app.config['LOCAL_MONGO_URL'])
+db = conn.fearthquakes
+
 
 @app.route('/')
 def main_page():
